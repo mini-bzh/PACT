145fad10d20b89767028780b01dd9087bdbee8b0
blambertbrun <blambertbrun@etudiant.univ-rennes1.fr>
2024-10-16 08:44:59 +0200

    trucs

--- gestionOffres.php
+++ gestionOffres.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestion des offres</title>
 </head>
-<body>
-    
-</body>
+    <body>
+        
+    </body>
 </html>
