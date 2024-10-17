8453016054f36f8e54a2beb17f49f92c9f15d6ce
Baitaaaa <133127200+Baitaaaa@users.noreply.github.com>
2024-10-17 15:15:28 +0200

    modif header PC
    ajout de classes et changement de nom

---
+++ html/composants/header/headerPC-Tab.php
@@ -0,0 +1,40 @@
+ 
+
+    <header>
+        <nav class="navHeader">
+            <img class="logoHeader" src="../../images/logo/logo_grand.png" alt="logo PACT">
+            <ul class="ulHeader">
+                <li class="liHeader" id="btAccueil">
+                    <a class="aHeader" href="">
+                    <img src="../../icones/homeSVG.svg" alt="icone home">
+                    <h3>Accueil</h3>
+                    </a>
+                </li>
+                <li class="liHeader" id="btRech">
+                    <a class="aHeader" href="">
+                    <img src="../../icones/rechercherSVG.svg" alt="icone rechercher">
+                    <h3>Rechercher</h3>
+                    </a>
+                </li>
+                <li class="liHeader" id="btAvis">
+                    <a class="aHeader" href="">
+                    <img src="../../icones/commentSVG.svg" alt="icone commentaires">
+                    <h3>Mes avis</h3>
+                    </a>
+                </li>
+                <li class="liHeader" id="btOffres" hidden>
+                    <a class="aHeader" href="">
+                    <img src="../../icones/offreSVG.svg" alt="icone offres">
+                    <h3>Mes offres</h3>
+                    </a>
+                </li>
+                <li class="liHeader" id="btCompte">
+                    <a class="aHeader" href="">
+                    <img src="../../icones/compteSVG.svg" alt="icone compte">
+                    <h3>Mon compte</h3>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header> 
+
