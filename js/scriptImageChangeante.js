3632c3d17946dc3b909068c9114c4db005eba88f
blambertbrun <blambertbrun@etudiant.univ-rennes1.fr>
2024-10-18 11:33:01 +0200

    progrès responsive détail offre

--- js/scriptImageChangeante.js
+++ js/scriptImageChangeante.js
@@ -3,13 +3,21 @@ let tabImages = ["carrou_fort1.jpg", "carrou_fort2.jpg", "carrou_fort3.jpg"];
 let index = 0;
 function changeImage()
 {
-    index = (index + 1) % tabImages.length;
-    document.getElementById("imageChangeante").src = "../images/images_illsutration_tempo/fort_la_latte/" + tabImages[index];
+    let image = document.getElementById("imageChangeante");
+
+    image.style.opacity = 0;
+
+    setTimeout(() => {
+        index = (index + 1) % tabImages.length;
+
+        image.src = "../images/images_illsutration_tempo/fort_la_latte/" + tabImages[index];
+
+        image.style.opacity = 1;
+
+    }, 300);
+
 }
-
-
 
 setInterval(() => {
     changeImage();
 }, 2000);
-
