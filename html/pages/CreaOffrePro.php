baff21b753ee7b63df50def468929e550611da39
MateoMorvan <mateo.morvan.2205@gmail.com>
2024-10-18 15:00:25 +0200

    CreaOffreP1

--- html/pages/CreaOffrePro.php
+++ html/pages/CreaOffrePro.php
@@ -5,6 +5,7 @@
 
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,56 +13,63 @@
     <link rel="stylesheet" href="../style/style.css">
     <link rel="stylesheet" href="../style/pages/CreaOffrePro.css">
 </head>
+
 <body>
     <h1></h1>
 
     <div>
         <h2>Création d'une offre</h2>
+        <div id="test">
+            <form name="test" action="http://localhost:8888/import.php" method="get">
 
-        <form name="test" action="http://localhost:8888/import.php" method="get">
+                <div class="champTexte">
+                    <label for="titre">Titre :</label>
+                    <input type="text" id="titre" name="titre" required />
+                    <br />
+                </div>
 
-         <!-- Texte avec label -->
-         <label for="titre">Titre :</label>
-         <input type="text" id="titre" name="titre" required />
-         <br />
+                <div class="champTexte">
+                    <label for="categorie">Catégorie :</label>
+                    <select id="categorie" name="categorie">
+                        <option value="restauration">Restauration</option>
+                        <option value="visite">Visite</option>
+                        <option value="spectacle">Spectacle</option>
+                        <option value="parcDatraction">Parc d'attraction</option>
+                        <option value="activite">Activité</option>
+                    </select>
+                    <br />
+                </div>
 
-        <!-- Liste déroulante -->
-        <label for="categorie">Catégorie :</label>
-        <select id="categorie" name="categorie">
-            <option value="restauration">Restauration</option>
-            <option value="visite">Visite</option>
-            <option value="spectacle">Spectacle</option>
-            <option value="parcDatraction">Parc d'attraction</option>
-            <option value="activite">Activité</option>
-        </select>
-        <br />
+                <?php
 
-        <?php
+                ?>
+                <div class="champTexte">
+                    <!-- Liste déroulante -->
+                    <label for="tags">Tags :</label>
+                    <select id="tags" name="tags">
+                        <option value="FR">Française</option>
+                        <option value="hotel">Hotel</option>
+                        <option value="chateau">Chateau</option>
+                        <option value="musee">Musée</option>
+                        <option value="visite">Visite</option>
+                        <option value="chambrehote">Chambre d'hôtes</option>
+                        <option value="autre">Autre</option>
+                    </select>
+                    <br />
+                </div>
+        </div>
+        <div>
+            <label for="resume">Résumé :</label>
+            <textarea id="resume" name="resume" rows="5" cols="33" placeholder="Résumé de votre offre"></textarea>
+            <br />
 
-        ?>
+            <label for="descripDetail">Description détaillé :</label>
+            <textarea id="resume" name="resume" rows="10" cols="60" placeholder="Description détaillé de votre offre"></textarea>
+            <br />
+        </div>
 
-        <!-- Liste déroulante -->
-        <label for="tags">Tags :</label>
-        <select id="tags" name="tags">
-            <option value="FR">Française</option>
-            <option value="hotel">Hotel</option>
-            <option value="chateau">Chateau</option>
-            <option value="musee">Musée</option>
-            <option value="visite">Visite</option>
-            <option value="chambrehote">Chambre d'hôtes</option>
-            <option value="autre">Autre</option>
-        </select>
-        <br />
-
-        <label for="resume">Résumé :</label>
-        <textarea id="resume" name="resume" rows="5" cols="33" placeholder="Résumé de votre offre"></textarea>
-        <br />
-
-        <label for="descripDetail">Description détaillé :</label>
-        <textarea id="resume" name="resume" rows="10" cols="60" placeholder="Description détaillé de votre offre"></textarea>
-        <br />
-
-        </form> 
+        </form>
     </div>
 </body>
+
 </html>
