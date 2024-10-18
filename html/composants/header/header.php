fe3dfa7492a4dbad62c529e439eee833b1930959
Baitaaaa <133127200+Baitaaaa@users.noreply.github.com>
2024-10-18 11:56:39 +0200

    ajout du header pour mobile

---
+++ html/composants/header/header.php
@@ -0,0 +1,77 @@
+ 
+
+    <header class="headerPC-Tab">
+        <nav class="navHeader">
+            <img class="logoHeader" src="../../images/logo/logo_grand.png" alt="logo PACT">
+            <ul class="ulHeader">
+                <li class="liHeader" id="btAccueil">
+                    <a class="aHeader" href="">
+                    <img src="../../icones/homeSVG.svg" alt="icone home">
+                    <h3>Accueil</h3>
+                    </a>
+                </li>
+                <li class="liHeader" id="btRech">
+                    <a class="aHeader" href="">
+                    <img src="../../icones/rechercherSVG.svg" alt="icone rechercher">
+                    <h3>Rechercher</h3>
+                    </a>
+                </li>
+                <li class="liHeader" id="btAvis">
+                    <a class="aHeader" href="">
+                    <img src="../../icones/commentSVG.svg" alt="icone commentaires">
+                    <h3>Mes avis</h3>
+                    </a>
+                </li>
+                <li class="liHeader" id="btOffres" hidden>
+                    <a class="aHeader" href="">
+                    <img src="../../icones/offreSVG.svg" alt="icone offres">
+                    <h3>Mes offres</h3>
+                    </a>
+                </li>
+                <li class="liHeader" id="btConnect" hidden>
+                    <a class="aHeader" href="">
+                    <img src="../../icones/compteSVG.svg" alt="icone compte">
+                    <h3>Se connecter</h3>
+                    </a>
+                </li>
+                <li class="liHeader" id="btCompte">
+                    <a class="aHeader" href="">
+                    <img src="../../icones/compteSVG.svg" alt="icone compte">
+                    <h3>Mon compte</h3>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header> 
+
+    <header class="headerMobile">
+        <nav class="navHeaderMobile">
+            <ul class="ulHeaderMobile">
+                <li class="liHeaderMobile" id="btAccueilMobile">
+                    <a class="aHeaderMobile" href="">
+                    <img src="../../icones/homeSVG.svg" alt="icone home">
+                    <div class="trait" hidden></div>
+                    </a>
+                </li>
+                <li class="liHeaderMobile" id="btRechMobile">
+                    <a class="aHeaderMobile" href="">
+                    <img src="../../icones/rechercherSVG.svg" alt="icone rechercher">
+                    <div class="trait"></div>
+                    </a>
+                </li>
+                <li class="liHeaderMobile" id="btAvisMobile">
+                    <a class="aHeaderMobile" href="">
+                    <img src="../../icones/commentSVG.svg" alt="icone commentaires">
+                    <div class="trait" hidden></div>
+                    </a>
+                </li>
+                <li class="liHeaderMobile" id="btCompteMobile">
+                    <a class="aHeaderMobile" href="">
+                    <img src="../../icones/compteSVG.svg" alt="icone compte">
+                    <div class="trait" hidden></div>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+
