2314e89442964f5f6e18f3d2ed7939f93cd75589
mini_bzh <mini.h.13.9.14.9@gmail.com>
2024-10-24 00:41:15 +0200

    avis session gestion

--- html/pages/avis.php
+++ html/pages/avis.php
@@ -1,9 +1,16 @@
 <?php
-    $profil = null;
-    if(key_exists("user", $_GET))
-    {
-        $profil =$_GET["user"];
-    }
+    session_start(); // recuperation de la sessions
+
+    // recuperation des parametre de connection a la BdD
+    include('/var/www/html/php/connection_params.php');
+    
+    // connexion a la BdD
+    $dbh = new PDO("$driver:host=$server;dbname=$dbname", $user, $pass);
+    $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC); // force l'utilisation unique d'un tableau associat
+
+    // cree $comptePro qui est true quand on est sur un compte pro et false sinon
+    include('/var/www/html/php/verif_compte_pro.php');
+
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -11,10 +18,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Avis</title>
-    <link rel="stylesheet" href="../style/pages/avis.css">
+    <link rel="stylesheet" href="/style/pages/avis.css">
 </head>
 <body class=<?php                          //met le bon fond en fonction de l'utilisateur
-            if ($profil == "pro")
+            if ($comptePro)
             {
                 echo "fondPro";
             }
@@ -24,13 +31,13 @@
             }
     ?>>
         
-    <?php include "../composants/header/header.php";        //import header (navbar)
+    <?php include "/var/www/html/composants/header/header.php";        //import header (navbar)
     ?>
     <main>
         <h1>Coming soon !</h1>
     </main>
     <?php                                                   //import footer
-            include "../composants/footer/footer.php";
+            include "/var/www/html/composants/footer/footer.php";
     ?>
 </body>
 </html>
