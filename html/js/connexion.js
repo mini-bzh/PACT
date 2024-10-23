3607c4f50c49a16cd2108feced623281eaddce87
yaitouazzou <yaitouazzou@etudiant.univ-rennes1.fr>
2024-10-23 16:39:57 +0200

    test correction pop up connexion 4

--- html/js/connexion.js
+++ html/js/connexion.js
@@ -2,10 +2,12 @@
 function validerCorrect() {
     let pop = document.getElementsByClassName('popUp');
     pop.style.display = 'flex';
+    document.body.classList.add('no-scroll');
 
     // Après 5 secondes, cacher la popup et réactiver les interactions
     setTimeout(function() {
         pop.style.display = 'none';
+        document.body.classList.remove('no-scroll');
         header("Location: https://tripskell.ventsdouest.dev/pages/accueil.php?user=pro");
         exit;
     }, 5000);
