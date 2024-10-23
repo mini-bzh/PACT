fc408de92acceb21554ed1411381b6a21a4492a8
blambertbrun <blambertbrun@etudiant.univ-rennes1.fr>
2024-10-23 10:48:25 +0200

    update page recherche

--- html/pages/recherche.php
+++ html/pages/recherche.php
@@ -1,3 +1,21 @@
+<?php
+    $profil = null;
+    if(key_exists("user", $_GET))
+    {
+        $profil =$_GET["user"];
+    }
+
+$driver = "pgsql";
+
+$server = "postgresdb";
+$dbname = "postgres";
+
+$user = "sae";
+$pass = "ashton-izzY-c0mplet";
+
+$dbh = new PDO("$driver:host=$server;dbname=$dbname", $user, $pass);
+
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
