d484eae59dc2a1947fd89c51d5c642b74c182b5d
blambertbrun <blambertbrun@etudiant.univ-rennes1.fr>
2024-10-23 16:30:54 +0200

    test exécution php avec js

--- html/pages/toggleStatutOffre.php
+++ html/pages/toggleStatutOffre.php
@@ -1,4 +1,9 @@
 <?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+
     $profil = null;
     if(key_exists("user", $_GET))
     {
@@ -14,11 +19,20 @@
     $pass = "ashton-izzY-c0mplet";
 
     $dbh = new PDO("$driver:host=$server;dbname=$dbname", $user, $pass);
-
-
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') 
+    {
+        // Récupérer les paramètres envoyés via POST
     $idOffre = $_POST["idOffre"];
 
     $statut = $dbh->query("SELECT enLigne from tripskell.offrepro WHERE idoffre = $idOffre);
+    echo $statut;
+    } 
+    else 
+    {
+        echo "pb";
+    }
+
+
 
     echo $statut;
 ?>
