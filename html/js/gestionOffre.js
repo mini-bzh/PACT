fee969c96ccd6b6da8c6dfb61b9af541413a5d59
blambertbrun <blambertbrun@etudiant.univ-rennes1.fr>
2024-10-24 00:26:59 +0200

    test en ligne ou non gestionOffres

--- html/js/gestionOffre.js
+++ html/js/gestionOffre.js
@@ -1,7 +1,8 @@
 function ChangerBtnLigne(idOffre) 
 {
     //const image = document.getElementById('imgEnHorsLigne');
-    const image = document.querySelectorAll("#offre" + idOffre + " #conteneurBtnGestion img");
+    const image = document.querySelector("#offre" + idOffre + " #conteneurBtnGestion img");
+    console.log(document.querySelector("#offre4"));
     console.log("#offre" + idOffre + " .conteneurBtnGestion img");
     if (image.src.includes("/icones/horsLigneSVG.svg")) 
     {
