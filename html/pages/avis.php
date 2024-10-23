c22e1ad3c227621f8661e1fb9e0c8bfbff96a2d9
blambertbrun <blambertbrun@etudiant.univ-rennes1.fr>
2024-10-23 11:36:58 +0200

    changé chemin style avis.css

--- html/pages/avis.php
+++ html/pages/avis.php
@@ -11,7 +11,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Avis</title>
-    <link rel="stylesheet" href="../style/style.css">
+    <link rel="stylesheet" href="../style/pages/avis.css">
 </head>
 <body class=<?php                          //met le bon fond en fonction de l'utilisateur
             if ($profil == "pro")
