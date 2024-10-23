9efd8b42347e970e96d12b0e5af1027927578b9f
KurosakiGoku <cahagneethan@gmail.com>
2024-10-23 11:01:36 +0200

    gestionOffres modif programmes

--- html/pages/gestionOffres.php
+++ html/pages/gestionOffres.php
@@ -44,48 +44,43 @@
                     </div>
                 </a>
                 <article class="offre">
-                    <h2>Fort la Latte</h2>
-                    <p>Visite</p>
+                    <h2><?php echo $contentOffre["titreoffre"]?></h2>
+                    <!-- <p>Visite</p> future categorie -->
                     <div class="conteneurSpaceBetween">
                         <div class="noteDetailOffre">
                             <div class="etoiles">
-                                <p>4.7</p>
-                                <img src="../icones/etoilePleineSVG.svg" alt="etoile pleine">
-                                <img src="../icones/etoilePleineSVG.svg" alt="etoile pleine">
-                                <img src="../icones/etoilePleineSVG.svg" alt="etoile pleine">
-                                <img src="../icones/etoilePleineSVG.svg" alt="etoile pleine">
-                                <img src="../icones/etoileMoitiePleineSVG.svg" alt="etoile moitié pleine">
+                                <p><?php echo $contentOffre["note"];?></p>
+                                <?php include "/var/www/html/php/etoiles.php"; ?>
                             </div>
-                            <p>38 avis</p>
+                            <!-- <p>38 avis</p> -->
                         </div>
                         <div class="conteneurSVGtexte">
                             <img src="../icones/logoUserSVG.svg" alt="pro">
-                            <p>Fort la Latte</p>
+                            <p><?php echo $dbh->query("select raison_social from tripskell._professionnel as p where p.id_c='" . $contentOffre["id_c"] . "';")->fetchAll()[0]["raison_social"]; ?></p>
                         </div>
                     </div>
 
                     <div class="imgChg">
-                        <img src="../images/images_illsutration_tempo/fort_la_latte/carrou_fort1.jpg" alt="" id="imageChangeante">
+                        <img src="/images/imagesOffres/<?php echo $contentOffre["img1"]; ?>" alt="" id="imageChangeante">
                     </div>
                     <div class="resumePrixDetailOffre">
-                        <p>Le chateau de la roche, Fort la Latte, situé à Plévenon cap Fréhel</p>
+                        <p><?php echo $contentOffre["resume"];?></p>
                         <hr>
-                        <p>À partir de 7.50€/pers</p>
+                        <!-- Tarif minimal -->
+                        <p>À partir de <?php echo $contentOffre["tarifminimal"];?>€/pers</p>
                     </div>
 
                     <p id="descriptionOffre">
-                    Le Fort La Latte, construit au XIVe siècle, est un château fort majestueux situé en Bretagne, sur une falaise face à 
-                    la mer. Entouré de remparts et de tours, il surplombe la Côte d'Émeraude et offre des panoramas spectaculaires. Ce 
-                    lieu emblématique attire de nombreux visiteurs pour son histoire et son cadre pittoresque.
+                    <?php echo $contentOffre["description_detaille"]; ?>
                     </p>
                 
                     <div class="conteneurSpaceBetween" id="conteneurTagsHoraires">
-                        <div id="partieTags">
+                        <!-- future tag<div id="partieTags">
                             <div class="conteneurSVGtexte">
-                                <img src="../icones/tagSVG.svg" alt="icone tag">
+                                <img src="/icones/tagSVG.svg" alt="icone tag">
                                 <h4>Tags</h4>
                             </div>
-                            <hr>
+                            <hr> 
                             <div id="conteneurTagsOffre">
                                 <p class="tagOffre">Culturel</p>
                                 <p class="tagOffre">Histoire</p>
@@ -94,13 +89,13 @@
 
 
                             </div>
-                        </div>
+                        </div> -->
                         <div id="partieHoraires">
                             <div class="conteneurSVGtexte">
-                                <img src="../icones/horairesSVG.svg" alt="icone horaires">
+                                <img src="/icones/horairesSVG.svg" alt="icone horaires">
                                 <h4>Horaires</h4>
                             </div>
-                            <hr>
+                            <hr><!-- future jours d'ouverture
                             <div id="conteneurJoursOffre">
                                 <p class="jour jourOuvert">L</p>
                                 <p class="jour jourOuvert">Ma</p>
@@ -109,17 +104,19 @@
                                 <p class="jour jourOuvert">V</p>
                                 <p class="jour jourFerme">S</p>
                                 <p class="jour jourFerme">D</p>
-                            </div>
-                                <div id="conteneurPlagesHoraires">
-                                    <p class="plageHoraire">De <span class="horaireEncadre">07h30</span> à <span class="horaireEncadre">19h00</span></p>
+                            </div>-->
+
+                            <!-- Horaires -->
+                            <div id="conteneurPlagesHoraires">
+                                <p class="plageHoraire">De <span class="horaireEncadre"><?php echo explode("-",$contentOffre["horaires"])[0]; ?></span> à <span class="horaireEncadre"><?php echo explode("-",$contentOffre["horaires"])[1]; ?></span></p>
                             </div>
                         </div>
                     </div>
-                    <a href="https://www.google.com/maps/search/?api=1&query=Fort%20La%20Latte%20-%20Ch%C3%A2teau%20de%20la%20Roche%20Goyon"
-                    class="conteneurSVGtexte grossisQuandHover" id="itineraire" target="_blank">
-                        <img src="../icones/adresseSVGblanc.svg" alt="icone adresse">
+                    <!-- <a href="https://www.google.com/maps/search/?api=1&query=Fort%20La%20Latte%20-%20Ch%C3%A2teau%20de%20la%20Roche%20Goyon"
+                    class="conteneurSVGtexte" id="itineraire" target="_blank">
+                        <img src="/icones/adresseSVGblanc.svg" alt="icone adresse">
                         <p>Itinéraire</p>
-                    </a>
+                    </a> -->
                     <hr id="separateurOffreGestion">
                     <div id="conteneurGestion">
                         <h4>Statut : <span class="enLigne">en ligne</span></h4>
