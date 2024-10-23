0c8d1ed21871aacfc08d0171beac9a3459648cfe
blambertbrun <blambertbrun@etudiant.univ-rennes1.fr>
2024-10-23 16:23:26 +0200

    test envoi requête bdd à partir de js

--- html/pages/test2.php
+++ html/pages/test2.php
@@ -1,15 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
 </head>
 <body>
-    <h1>cc</h1>
-</body>
-</html>
-<!--
 <button id="monBouton">Exécuter PHP</button>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -29,4 +25,6 @@
         });
     });
 </script>
--->
+
+</body>
+</html>
