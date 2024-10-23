e14dd32380f68d588a9a8039025e13be6b2c4fe0
yaitouazzou <yaitouazzou@etudiant.univ-rennes1.fr>
2024-10-23 18:29:15 +0200

    test de debugage page de connexion 8

--- html/pages/connexion.php
+++ html/pages/connexion.php
@@ -52,11 +52,10 @@ if (($correspond === false) && (count($result) === 0) && (count($result2) === 0)
     $message1 = "<p style='color:red;'>Ce nom d'utilisateur n'existe pas.</p>";
 }
 
-if (($correspond === false) && ((isset($_POST['userName'])) && (isset($_POST['userPSW']))) && ((count($result) === 1) && (count($result2) === 1))){
+if (($correspond === false) && ((isset($_POST['userName'])) && (isset($_POST['userPSW']))) && ((count($result) === 1) || (count($result2) === 1))){
     $message2 = "<p style='color:red;'>Mot de passe incorrect.</p>";
 }
-print(count($result));
-print(count($result2));
+
 ?>
 
 
