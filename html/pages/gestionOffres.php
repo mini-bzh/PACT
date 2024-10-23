861d794aeab36af6f913ef33e00cba805da13c91
KurosakiGoku <cahagneethan@gmail.com>
2024-10-23 14:52:42 +0200

    gestionOffres test probleme

--- html/pages/gestionOffres.php
+++ html/pages/gestionOffres.php
@@ -118,9 +118,9 @@
                     </a> -->
                     <hr id="separateurOffreGestion">
                     <div id="conteneurGestion">
-                        <h4>Statut : <span class="<?php if($contentOffre["enLigne"]=="true"){echo "enLigne";} else{echo "horsLigne";} ?>" id="txtEnLigne">en Ligne</span></h4>
+                        <h4>Statut : <span class="<?php if($contentOffre["enLigne"]=="true"){echo "enLigne";} else{echo "horsLigne";} ?>">en Ligne</span></h4>
                         <div id="conteneurBtnGestion">
-                            <div class="btnGestionOffre grossisQuandHover" id="btnEnHorsLigne"  onclick="ChangerBtnLigne">
+                            <div class="btnGestionOffre grossisQuandHover" id="btnEnHorsLigne"  onclick="ChangerBtnLigne()">
                                 <img src="../icones/horsLigneSVG.svg" alt="" id="imgEnHorsLigne">
                                 <p id="txtEnHorsLigne">Mettre l'offre hors ligne</p>
                             </div>
