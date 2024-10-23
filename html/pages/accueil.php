7be7a02165af39d211955432550d013357b00ad4
blambertbrun <blambertbrun@etudiant.univ-rennes1.fr>
2024-10-23 10:00:49 +0200

    test images

--- html/pages/accueil.php
+++ html/pages/accueil.php
@@ -65,7 +65,7 @@ $dbh = new PDO("$driver:host=$server;dbname=$dbname", $user, $pass);
                                         </div>
                             
                                         <div class="conteneurImage">
-                                            <img src="/images/imagesOffres/" alt="illustration offre">
+                                            <img src="/images/imagesOffres/<?php echo $row["img1"]?>" alt="illustration offre">
                                             <div class="text-overlay">dès <?php echo $row["tarifminimal"]?>€/pers</div>
                                         </div>
                                         
