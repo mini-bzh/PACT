ff90813801f46bcd4eb13f2c4751035d7bf8c623
blambertbrun <blambertbrun@etudiant.univ-rennes1.fr>
2024-10-24 17:17:23 +0200

    test table pro public

--- html/pages/test.php
+++ html/pages/test.php
@@ -5,12 +5,12 @@ include('/var/www/html/php/connection_params.php');
 // connexion a la BdD
 $dbh = new PDO("$driver:host=$server;dbname=$dbname", $user, $pass);
 $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-$stmt = $dbh->prepare("select * from tripskell.offre_pro");
+$stmt = $dbh->prepare("select * from tripskell.pro_public");
 $stmt->execute();
 $result = $stmt->fetchAll();
 
 foreach ($result as $row)
 {
     print_r($row);
-    ?><br><?php
+    ?><br><br><?php
 }
