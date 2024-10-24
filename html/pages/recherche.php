03af3e5f5fe05df3c5fa94acee39e0afbf38c12c
blambertbrun <blambertbrun@etudiant.univ-rennes1.fr>
2024-10-24 11:08:36 +0200

    update recherche

--- html/pages/recherche.php
+++ html/pages/recherche.php
@@ -11,16 +11,34 @@
     // cree $comptePro qui est true quand on est sur un compte pro et false sinon
     include('/var/www/html/php/verif_compte_pro.php');
 
+    if($comptePro)      /* prépare la requête pour récupérer les offres à afficher : offres du pro si connecté en tant que pro, toutes les 
+                         offres sinon */
+
+    {
+        $stmt = $dbh->prepare("select * from tripskell.offre_pro where id_c=:id_c");
+
+        // binding pour l'id du compte (id_c <- idCompte(dans $_SESSION))
+        $stmt->bindParam(":id_c", var: $id_c); 
+        $id_c = $_SESSION["idCompte"];
+    }
+    else
+    {
+        $stmt = $dbh->query("SELECT * from tripskell.offre_pro WHERE enligne = true");
+    }
+
+    $stmt->execute();
+    $rows = $stmt->fetchAll();          // rows : les offres à afficher
+
 ?>
 <!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Recherche</title>
-    <link rel="stylesheet" href="/style/pages/recherche.css">
-</head>
-<body class=<?php                          //met le bon fond en fonction de l'utilisateur
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Rechercher</title>
+        <link rel="stylesheet" href="/style/pages/recherche.css">
+    </head>
+    <body  class=<?php                          //met le bon fond en fonction de l'utilisateur
             if ($comptePro)
             {
                 echo "fondPro";
@@ -30,17 +48,69 @@
                 echo "fondVisiteur";
             }
         ?>>
-        <?php include "/var/www/html/composants/header/header.php";        //import header (navbar)
+        <?php include "/var/www/html/composants/header/header.php";        //import navbar
         ?>
+        <div class="titrePortable">
+
+            <svg width="401" height="158" viewBox="0 0 401 158" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g filter="url(#filter0_d_169_4380)">
+            <ellipse cx="169.5" cy="61" rx="231.5" ry="89" fill="white"/>
+            </g>
+            <defs>
+            <filter id="filter0_d_169_4380" x="-66" y="-28" width="471" height="186" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+            <feOffset dy="4"/>
+            <feGaussianBlur stdDeviation="2"/>
+            <feComposite in2="hardAlpha" operator="out"/>
+            <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+            <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_169_4380"/>
+            <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_169_4380" result="shape"/>
+            </filter>
+            </defs>
+            </svg>
+
+        <div>
+    <img src="/images/logo/logo_grand.png" alt="logo grand" id="logoTitreMobile">
+</div>
+</div>
         <main>
-            <h1>Toutes les offres</h1>
+            <?php
+                if($comptePro)
+                {
+                    ?>
+                    <h1>Mes offres</h1>
+                    <?php
+                }
+                else
+                {
+                    ?>
+                    <h1>Toutes les offres</h1>
+                    <?php
+                }
+            ?>
+
             <section id="conteneurOffres">
                 <article>
                     <?php
-                        foreach($dbh->query("SELECT * from tripskell.offre_visiteur") as $row) 
+                        foreach($rows as $row)          // parcourt les offres pour les afficher
                         {
                             ?>
-                                <a href="/pages/detailOffre.php?idOffre=<?php echo $row["idoffre"]?>" class="lienApercuOffre grossisQuandHover">
+                                <a <?php
+                                    if($comptePro)
+                                    {
+                                        ?>
+                                            href="/pages/gestionOffres.php/#offre<?php echo $row['idoffre'];?>"
+                                        <?php
+                                    }
+                                    else
+                                    {
+                                        ?>
+                                            href="/pages/detailOffre.php?idOffre=<?php echo $row["idoffre"]?>"
+                                        <?php
+                                    }
+                                ?>
+                                href="/pages/detailOffre.php?idOffre=<?php echo $row["idoffre"]?>" class="lienApercuOffre grossisQuandHover">
                                     <article class="apercuOffre">
                                         <h3><?php echo $row["titreoffre"]?></h3>
                                         <div class="conteneurSVGtexte">
@@ -82,8 +152,8 @@
                 </article>
             </section>
         </main>
-        <?php                                                   //import footer
-            include "/var/www/html/composants/footer/footer.php";
+        <?php
+            include "../composants/footer/footer.php";
         ?>
-</body>
+    </body>
 </html>
