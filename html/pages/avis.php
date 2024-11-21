8570ef2e42a336d90844ce666c1737fd96efa04d
blambertbrun <blambertbrun@etudiant.univ-rennes1.fr>
2024-11-21 10:12:44 +0100

    ajout titre page compte quand connecté

--- html/pages/avis.php
+++ html/pages/avis.php
@@ -37,6 +37,32 @@
         
     <?php include "../composants/header/header.php";        //import header (navbar)
     ?>
+    <div class="titrePortable">
+
+        <svg width="401" height="158" viewBox="0 0 401 158" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g filter="url(#filter0_d_169_4380)">
+        <ellipse cx="169.5" cy="61" rx="231.5" ry="89" fill="white"/>
+        </g>
+        <defs>
+        <filter id="filter0_d_169_4380" x="-66" y="-28" width="471" height="186" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+        <feOffset dy="4"/>
+        <feGaussianBlur stdDeviation="2"/>
+        <feComposite in2="hardAlpha" operator="out"/>
+        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_169_4380"/>
+        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_169_4380" result="shape"/>
+        </filter>
+        </defs>
+        </svg>
+
+        <div id="conteneurTitreMobile">
+            <!--<img src="/images/logo/logo_grand.png" alt="logo grand" id="logoTitreMobile">-->
+            <img src="/images/logo/logo_petit.png" alt="logo petit" id="logoTitreMobile">
+            <h3>Mes avis</h3>
+        </div>
+    </div>
     <main>
         <h1>Coming soon !</h1>
     </main>
