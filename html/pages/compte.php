00fddb1551cc45fa028ea16be3d3fb050a580c47
yaitouazzou <yaitouazzou@etudiant.univ-rennes1.fr>
2024-11-25 18:23:19 +0100

    ajout de la consultation des informations de comptes, manque les boutons et mode de payement

--- html/pages/compte.php
+++ html/pages/compte.php
@@ -123,24 +123,190 @@ if ((!$comptePro) && (!$compteMembre)) {
 
 </main>
 
-<!-- SI C'EST UN PROFESSIONNEL !!! -->
+<!-- SI C'EST UN UTILISATEUR CONNECTÉ !!! -->
 
 <?php
-} else if (($comptePro) || ($compteMembre)) {
+} else {
 ?>
 
 <!------ MAIN  ------>
 <main>
 
-<button class="btnDeconnexion" onclick="confDeco()">
 <?php
-    include '../icones/deconnexionSVG.svg';
+
+    $dbh = new PDO("$driver:host=$server;dbname=$dbname", $user, $pass);
+    $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC); // force l'utilisation unique d'un tableau associat
+
+    $idCompte = $_SESSION['idCompte'];
+
+    if ($comptePro) {
+
+        $stmt = $dbh->prepare("SELECT * from tripskell.pro_prive where id_c = :id");
+
+        $stmt->bindParam(':id', $idCompte, PDO::PARAM_STR);
+
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+
+
+        if (count($result) === 0) {
+
+            $stmt = $dbh->prepare("SELECT * from tripskell.pro_public where id_c = :id");
+
+            $stmt->bindParam(':id', $idCompte, PDO::PARAM_STR);
+
+            $stmt->execute();
+            $result = $stmt->fetchAll();
+
+        }
+    } else {
+        
+        $stmt = $dbh->prepare("SELECT * from tripskell.membre where id_c = :id");
+
+        $stmt->bindParam(':id', $idCompte, PDO::PARAM_STR);
+
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+
+    }
+
+    $infos = $result[0];
+
+
 ?>
-    <p class="boldArchivo">Déconnexion</p>
-</button>
+
+<!-- div principale -->
+<div class="informationsCompte">
+
+    <!-- div des informations de compte -->
+    <div class="zoneInfos">
+
+        <!-- div de l'image et de l'identité -->
+        <div class="infoId">
+            <img class="circular-image" src="../images/pdp/<?php echo $infos['pdp'] ?>" alt="Photo de profil" title="Photo de profil">
+<?php
+            if ($compteMembre){  // Si c'est un membre on affiche son nom / prénom / login
+?>
+            <div>
+                <p class="boldArchivo titreLogin"><?php echo $infos["login"] ?></p>
+                <div>
+                    <p><span class="boldArchivo">Nom : </span><?php echo $infos["nom"] ?></p>
+                    <p><span class="boldArchivo">Prenom : </span><?php echo $infos["prenom"] ?></p>
+                </div>
+            </div>
+<?php
+            } else {
+?>
+            <div>
+                <p class="boldArchivo titreLogin"><?php echo $infos["raison_social"] ?></p>
+<?php
+                $siren = trim(chunk_split($infos["num_siren"], 3, " ")); // On ajoute un esapce entre 3 caractères
+?>
+                <p><span class="boldArchivo">Numéro SIREN : </span><?php echo $siren ?></p>
+            </div>
+<?php
+            }
+?>
+        </div>
+
+        <!-- div du reste des informations -->
+        <div>
+
+        <!-- On récupère la date au bon format de la date de création d'une offre-->
+<?php
+            // On récupère la date
+            $dateString = $infos["date_crea_compte"];
+
+            // On créer un objet DateTime à partir de la chaîne
+            $date = new DateTime($dateString);
+
+            // On formater la date au format dd/mm/aaaa
+            $formattedDate = $date->format('d/m/Y');
+?>
+            <p><span class="boldArchivo">Création compte : </span><?php echo $formattedDate ?></p>
+
+        <!-- On récupère le type du compte-->
+<?php
+        //Si c'est un membre, on met membre
+        if ($compteMembre) {
+            $tCompte = "Membre";
+        // Si c'est un professionnel
+        } else {
+            // On regarde si c'est un professionnel privé
+            if(count($dbh->query("select id_c from tripskell.pro_prive where id_c='" . $_SESSION["idCompte"] . "';")->fetchAll()[0]) !== 0) {
+                $tCompte = "Professionnel privé";
+
+            // Sinon c'est un professionnel public
+            } else {
+                $tCompte = "Professionnel public";
+            }
+        }
+?>
+            <p><span class="boldArchivo">Type de compte : </span><?php echo $tCompte ?></p>
+
+            <!-- Email -->
+            <p><span class="boldArchivo">E-mail : </span><?php echo $infos["adresse_mail"] ?></p>
+
+            <!-- Adresse -->
+<?php
+            if ($compteMembre) {
+?>
+            <p><span class="boldArchivo">Adresse postal : </span><?php echo $infos["codepostal"] ?></p>
+<?php
+            } else {
+
+            $adrPro = $infos["numero"] . " " . $infos["rue"] . ", " . $infos["ville"] . " " . $infos["codepostal"]; // Variable pour concaténer les informations de l'adresse d'un pro
+?>
+            <p><span class="boldArchivo">Adresse : </span><?php echo $adrPro ?></p>
+<?php
+            }
+?>
+            <!-- Téléphone -->
+<?php
+            $tel = trim(chunk_split($infos["numero_tel"], 2, " ")); // On rajoute un espace entre 2 caractères
+?>
+            <p><span class="boldArchivo">Téléphone : </span><?php echo $tel ?></p>
+
+            <!-- Mot de passe (caché) -->
+<?php
+            $cache = str_repeat("*", strlen($infos["mot_de_passe"])); // On créer une variable qui possède autant de "*" que le nombre de lettres du mdp
+?>
+            <p><span class="boldArchivo">Mot de passe : </span><?php echo $cache ?></p>
+
+<?php
+            // Si c'est un pro, on rajoute le mode de payement
+            if ($comptePro) {
+?>
+            <p><span class="boldArchivo">Mode de payement : </span><?php echo "plus tard" ?></p>
+<?php
+            }
+?>
+        </div>
+
+    </div>
 
 
-<!-- POP-UP -->
+    <!-- div des boutons de compte -->
+    <div class="zoneBtn">
+
+        <!-- Bouton de deconnexion -->
+        <button class="btnDeconnexion" onclick="confDeco()">
+        <?php
+            include '../icones/deconnexionSVG.svg';
+        ?>
+            <p class="boldArchivo">Déconnexion</p>
+        </button>
+
+    </div>
+
+</div>
+
+
+
+
+
+
+<!-- POP-UP de deconnexion -->
 <div class="popUpDeco">
     <div>
         <p>Êtes vous sur de vouloir vous déconnecter ?</p>
