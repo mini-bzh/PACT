ef1954b4d8244a8372c426f9760a9a31848810c8
yaitouazzou <yaitouazzou@etudiant.univ-rennes1.fr>
2024-11-27 09:40:39 +0100

    correction bug affichage changement de taille screen info compte

--- html/js/menuDeroulant.js
+++ html/js/menuDeroulant.js
@@ -18,3 +18,38 @@ btnDer.addEventListener('click', () => {
         el.classList.toggle('displayNone');
     });
 });
+
+window.addEventListener("resize", () => {
+    if (window.innerWidth > 428) {
+        if (menu.classList.contains("deroulement")) {
+            menu.classList.remove('deroulement');
+        }
+    }
+    if (window.innerWidth > 428) {
+        if (!der.classList.contains("displayNone")) {
+            der.classList.add('displayNone');
+        }
+    }
+    if (window.innerWidth > 428) {
+        if (der.classList.contains("menuDeplie")) {
+            der.classList.remove('menuDeplie');
+        }
+    }
+    if (window.innerWidth > 428) {
+        if (fleche.classList.contains("rotate180")) {
+            fleche.classList.remove('rotate180');
+        }
+    }
+    if (window.innerWidth > 428) {
+        if (!fleche2.classList.contains("displayNone")) {
+            fleche2.classList.add('displayNone');
+        }
+    }
+    if (window.innerWidth > 428) {
+        lesParg.forEach(el => {
+            if (!el.classList.contains("displayNone")) {
+                el.classList.add('displayNone');
+            }
+        });
+    }
+});
