e664c5831277cfec2ba4cae47468753dc6893ecc
Baitaaaa <baptistelaine321@gmail.com>
2024-11-29 10:40:19 +0100

    js trie Avis

--- html/js/detailOffre.js
+++ html/js/detailOffre.js
@@ -14,8 +14,14 @@ la valeur de l'information (exemple : "titre" => "Fort la Latte", "prix" => 15)*
         mapTempo.set("element", element);               //l'élément dans le DOM
         
         let date = document.querySelectorAll("#" + element.id + " .datePublication")[0].textContent;
-        date = date.substring(0, date.length-1);
-        mapTempo.set("date", parseInt(date));   //date
+        date = date.split("-");
+        let i = 0 ;
+        date.forEach(element => {
+            date[i] = parseInt(element);
+            i++;
+        });
+        console.log(date);
+        mapTempo.set("date", date);   //date
 
         mapAvisInfos.set(element.id, mapTempo);
     });
@@ -23,5 +29,85 @@ la valeur de l'information (exemple : "titre" => "Fort la Latte", "prix" => 15)*
     return mapAvisInfos;
 }
 
+let mapAvisInfos = initAvis();
+console.log(mapAvisInfos);
 
-console.log(initAvis());
+let trieDate = "";
+
+function trierDate() {
+    if (trieDate == "") {
+
+        // Trie le Tableau mapAvisInfos dans l'ordre croissant dans le tableau mapTrieAcs
+        let mapTrieAcs = new Map(
+            [...mapAvisInfos.entries()].sort((a,b) => {
+              // Tri selon la somme des éléments
+              const sumA = a[1].get("date").reduce((acc, val) => acc + val, 0);
+              const sumB = b[1].get("date").reduce((acc, val) => acc + val, 0);
+              return sumA - sumB; // Ordre croissant
+            })
+          );
+
+        
+        let index = 0;
+        // Parcour le tableau mapTrieAcs pour ajouter un attribut order dans le style des élément
+        mapTrieAcs.forEach((map, key, value)=>{
+
+            // Récupère l'élément dans la page
+            let elem = document.getElementById(mapTrieAcs.get(key).get("id"));
+            elem.style.order = index;   // Rajoute l'attribut css order égal à sa position dans le tableau 
+
+            // Rajoute une bordure bleu sur le bouton
+            // document.getElementById("btnTrieDate").style.border = "solid";
+            // document.getElementById("btnTrieDate").style.borderColor = "blue" ;
+
+            index++;
+        })
+        
+        trieDate = "asc";   // Modifie l'état du trie
+    }
+    else if(trieDate == "asc") {
+
+        // Trie le Tableau mapAvisInfos dans l'ordre décroissant dans le tableau mapTrieDesc
+        let mapTrieDesc = new Map(
+            [...mapAvisInfos.entries()].sort((a,b) => {
+              // Tri selon la somme des éléments
+              const sumA = a[1].get("date").reduce((acc, val) => acc + val, 0);
+              const sumB = b[1].get("date").reduce((acc, val) => acc + val, 0);
+              return sumB - sumA; // Ordre croissant
+            })
+          );
+
+        let index = 0;
+        // Parcour le tableau mapTrieDesc
+        mapTrieDesc.forEach((map, key, value)=>{
+
+            // Récupère l'élément dans la page
+            let elem = document.getElementById(mapTrieDesc.get(key).get("id"));
+            elem.style.order = index;   // Rajoute l'attribut css order égal à sa position dans le tableau 
+
+            // Rajoute une bordure bleu sur le bouton
+            // document.getElementById("btnTriePrix").style.border = "solid";
+            // document.getElementById("btnTriePrix").style.borderColor = "pink" ;
+            index++;
+        })
+        trieDate = "decs";  // Modifie l'état du trie
+    }
+    else if(trieDate == "decs"){
+        let index = 0;
+        // Parcour le tableau mapAvisInfos pour enlever le trie et remettre les offres dans l'ordre normale
+        mapOffresInfos.forEach((map, key, value)=>{
+
+            // Récupère l'élément dans la page
+            let elem = document.getElementById(mapOffresInfos.get(key).get("id"));
+            elem.style.order = index;   // Rajoute l'attribut css order égal à sa position dans le tableau 
+
+            // Rajoute une bordure bleu sur le bouton
+            //document.getElementById("btnTriePrix").style.border = "none";
+            index++;
+        })
+        trieDate = "";  // Modifie l'état du trie
+    }
+
+}
+
+
