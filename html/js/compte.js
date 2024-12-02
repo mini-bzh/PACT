4b2b6dd7ba3896fb2393f6c5d8691861554f445f
dyburel <dylan.burel.1@etudiant.univ-rennes1.fr>
2024-12-03 00:53:42 +0100

    modification compte membre qui marche plus

--- html/js/compte.js
+++ html/js/compte.js
@@ -6,3 +6,34 @@ let detailPrixDeplie = document.getElementsByClassName("detailPrixDeplie");
 btnDetailPrix.addEventListener('click', () => {
     detailPrixDeplie.classList.toggle('displayNone');
 });
+
+/*
+<script>
+// Mot de passe correct
+const correctPassword = <?php echo $infos['mot_de_passe'];?> ;
+
+document.getElementById("trigger-modifier").addEventListener("click", () => {
+// Affiche la popup de saisie du mot de passe
+document.getElementById("popup").style.display = "block";
+});
+
+function verifyPassword() {
+const inputPassword = document.getElementById('password').value;
+const errorMessage = document.getElementById('error-message');
+const popup = document.getElementById('popup');
+
+// Comparer le mot de passe entré avec celui qui est récupéré du serveur
+const correctPassword = "<?php echo $infos['mot_de_passe']; ?>"; // Passer la variable PHP au JavaScript
+
+if (inputPassword === correctPassword) {
+// Si le mot de passe est correct, on cache la popup
+popup.style.display = "none";
+window.location.href = "modifComptemembre.php";
+} else {
+// Affiche un message d'erreur si le mot de passe est incorrect
+errorMessage.style.display = "block";
+}
+}
+</script>
+
+*/
