a03358d8ca0c1b7e0c35e6fe4473bd2535ba63f5
MateoMorvan <mateo.morvan.2205@gmail.com>
2024-12-04 13:07:02 +0100

    Update facture et préparation de la liste des facture pour une offre

--- html/pages/listeFacture.php
+++ html/pages/listeFacture.php
@@ -11,35 +11,115 @@ $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC); // force l'u
 // cree $comptePro qui est true quand on est sur un compte pro et false sinon
 include('../php/verif_compte_pro.php');
 
-// Creation requete pour recuperer les offres
-// du professionnel connecte
-$stmt = $dbh->prepare("select * from tripskell.offre_pro where id_c=:id_c;");
+if (!isset($_SESSION["idCompte"])) {
+    header("Location: /pages/erreur404.php");
+    exit();
+}
 
 // binding pour l'id du compte (id_c <- idCompte(dans $_SESSION))
-$stmt->bindParam(":id_c", var: $id_c);
 $id_c = $_SESSION["idCompte"];
 
-$stmt->execute();   // execution de la requete
+if (key_exists("idCompte", $_SESSION)) {
+    // reccuperation de id_c de pro_prive 
+    $idproprive = $dbh->query("select id_c from tripskell.pro_prive where id_c='" . $_SESSION["idCompte"] . "';")->fetchAll()[0];
 
-// recuperation de la reponse et mise en forme
-$contentMesOffres = $stmt->fetchAll();
+    // reccuperation de id_c de pro_public
+    $idpropublic = $dbh->query("select id_c from tripskell.pro_public where id_c='" . $_SESSION["idCompte"] . "';")->fetchAll()[0];
+}
 
+
+// Récupération de l'identifiant de l'offre si présent dans l'URL
+$idOffre = null;
+if (key_exists("idOffre", $_GET)) {
+    $idOffre = $_GET["idOffre"]; // Récupération de l'identifiant de l'offre
+
+    // Récupération des détails de l'offre à partir de la base de données
+    $contentOffre = $dbh->query("SELECT * FROM tripskell.facture WHERE idOffre='" . $idOffre . "';")->fetchAll();
+}
+?>
+<?php
+if (in_array($_SESSION["idCompte"], $idproprive)) {
 ?>
 
-<!DOCTYPE html>
-<html lang="fr">
+    <!DOCTYPE html>
+    <html lang="fr">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>gestion des offre</title>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>gestion des offre</title>
 
-    <!-- Favicon -->
-    <link rel="icon" href="../icones/favicon.svg" type="image/svg+xml">
+        <!-- Favicon -->
+        <link rel="icon" href="../icones/favicon.svg" type="image/svg+xml">
 
-    <link rel="stylesheet" href="/style/pages/gestionOffres.css">
-</head>
-<body>
-    
-</body>
-</html>
+        <link rel="stylesheet" href="/style/pages/listeFacture.css">
+    </head>
+
+    <body class="fondPro">
+
+        <?php include "../composants/header/header.php";        //import navbar
+        ?>
+
+        <main>
+            <h1>Liste facture</h1>
+            <div class="divTab">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Facture</th>
+                        <th>Date facture</th>
+                        <th>Visualiser la facture</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    foreach ($contentOffre as $row){
+                    ?>
+                        <tr>
+                            <td><?php echo "Facture N°" . $row["id_facture"]; ?></td>
+                            <td><?php echo $row["date_creation"]; ?></td>
+                            <td></td>
+                        </tr>
+                    <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
+            </div>
+        </main>
+
+        <?php
+        include "../composants/footer/footer.php";
+        ?>
+    </body>
+
+    </html>
+<?php
+} else { // si id_c n'est pas dans pro_prive on génère une erreur 404.
+?>
+    <!DOCTYPE html>
+    <html lang="fr">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Creation Offre</title>
+        <link rel="stylesheet" href="/style/pages/CreaOffrePro.css">
+    </head>
+
+    <body class="fondPro">
+
+        <?
+        include "../composants/header/header.php";        //import navbar
+        ?>
+
+        <main>
+            <h1> ERROR 404 </h1>
+        </main>
+
+        <?php
+        include "../composants/footer/footer.php";
+        ?>
+    <?php
+}
+    ?>
