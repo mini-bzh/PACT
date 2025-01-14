50777e37d8976b455e2b8d9b5ac0a1dbfc6025d3
mini_bzh <mini.h.13.9.14.9@gmail.com>
2025-01-14 17:28:52 +0100

    popup verif info banc

--- html/js/compte.js
+++ html/js/compte.js
@@ -5,13 +5,44 @@ btnDetailPrix.addEventListener('click', () => {
     detailPrixDeplie.classList.toggle('displayNone');
 });
 
-function confModif() {
+function confModifProfil() {
     let pop = document.querySelector('.popUpModif');
     pop.style.display = 'flex';
     document.body.classList.add('no-scroll');
 }
 
+function confModifBanc() {
+    let pop = document.querySelector('.popUpModifBancaire');
+    pop.style.display = 'flex';
+    document.body.classList.add('no-scroll');
+}
 
+function verif_pass() {
+    let passwordInput = document.getElementById('password_for_banc');
+    let valeur = passwordInput.value;
+
+    let request = "_compte where mot_de_passe = '" + valeur + "' and id_c = '" + id_c + "';";
+        
+    $.ajax({
+        url: "../php/sql_request.php",              // Le fichier PHP à appeler, qui met à jour la BDD
+        type: 'POST',                               // Type de la requête (pour transmettre idOffre au fichier PHP)
+        data: {request: request},
+        success: function(response) {
+            let pass = response;
+            //alert(pass);                        // Affiche la réponse du script PHP si appelé correctement
+            //location.reload();
+            if(pass === "1") {
+                window.location.href = "../pages/ModifInfoBancaire.php";
+            } else {
+                document.getElementById('erreur_mdp').classList.remove('displayNone');
+            }
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log("Erreur AJAX : ", textStatus, errorThrown);
+            alert("Erreur lors de l'appel du script PHP : " + textStatus);
+        }
+    });
+}
 /*
 <script>
 // Mot de passe correct
