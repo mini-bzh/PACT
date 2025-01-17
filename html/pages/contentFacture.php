6684c15c7539c6d7f764c32b0d70890f1a494f22
mini_bzh <mini.h.13.9.14.9@gmail.com>
2025-01-17 09:07:45 +0100

    correction factures

--- html/pages/contentFacture.php
+++ html/pages/contentFacture.php
@@ -20,11 +20,17 @@ if (!isset($_SESSION["idCompte"])) {
 $id_c = $_SESSION["idCompte"];
 
 if (key_exists("idCompte", $_SESSION)) {
-    // reccuperation de id_c de pro_prive 
-    $idproprive = $dbh->query("select id_c from tripskell.pro_prive where id_c='" . $_SESSION["idCompte"] . "';")->fetchAll()[0];
+    // reccuperation de id_c de pro_prive
+    $result_query = $dbh->query("select id_c from tripskell.pro_prive where id_c='" . $_SESSION["idCompte"] . "';")->fetchAll();
+    if (!empty($result_query)) {
+        $idproprive = $result_query[0];
+    }
 
     // reccuperation de id_c de pro_public
-    $idpropublic = $dbh->query("select id_c from tripskell.pro_public where id_c='" . $_SESSION["idCompte"] . "';")->fetchAll()[0];
+    $result_query = $dbh->query("select id_c from tripskell.pro_public where id_c='" . $_SESSION["idCompte"] . "';")->fetchAll();
+    if (!empty($result_query)) {
+        $idpropublic = $result_query[0];
+    }
 }
 
 
