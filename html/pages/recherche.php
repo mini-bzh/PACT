f924e86a6d5eb8786ff90d87e17af82dc41b6384
Baitaaaa <baptistelaine321@gmail.com>
2025-03-18 13:56:43 +0100

    déplacement style map

--- html/pages/recherche.php
+++ html/pages/recherche.php
@@ -291,32 +291,6 @@
         <?php
             include "../composants/footer/footer.php";
         ?>
-        <style>
-            #map{
-                width: 90%;
-                height: 500px;
-                z-index: 0;
-            }
-
-            .leaflet-popup-content-wrapper .apercuOffre{
-                width: 100%;
-                height: 100%;
-                margin: 0;
-            }
-
-            .leaflet-popup-content-wrapper .leaflet-popup-content p{
-                margin : 0;
-            }
-
-            .leaflet-popup-content-wrapper .leaflet-popup-content{
-                margin : 0;
-            }
-
-            .leaflet-popup-content-wrapper .leaflet-popup-content a{
-                color: black;
-            }
-
-        </style>
         <script src="../js/recherche.js" ></script>
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
         <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
