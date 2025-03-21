dff8fa7c1fe4c44e7f38995fc9a59b2fdcc22966
Baitaaaa <baptistelaine321@gmail.com>
2025-03-21 14:53:07 +0100

    mainenant c'est le bon bouton

--- html/js/carte.js
+++ html/js/carte.js
@@ -97,24 +97,24 @@ function updateMap() {
     }
 }
 
-document.getElementsByClassName("leaflet-top leaflet-right")[0].innerHTML = `<div id="btnAgrandir" class="leaflet-control leaflet-bar"><svg width="100px" height="100px" viewBox="0 0 16 16" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="si-glyph si-glyph-arrow-resize-3">
+document.getElementsByClassName("leaflet-top leaflet-right")[0].innerHTML = `<div id="btnAgrandir" class="leaflet-control leaflet-bar"><svg width="100px" height="100px" viewBox="0 0 16 16" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="si-glyph si-glyph-arrow-resize-1">
     
-    <title>125</title>
-    
-    <defs>
+        <title>124</title>
+        
+        <defs>
 
-    </defs>
-        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-            <g fill="#434343">
-                <path d="M15.995,1.852 L14.133,0.00800000003 L11.107,2.988 L9.062,0.972 L9.062,6.875 L15.049,6.875 L12.973,4.828 L15.995,1.852 Z" class="si-glyph-fill">
+        </defs>
+            <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                <g fill="#434343">
+                    <path d="M6.995,10.852 L5.133,9.008 L2.107,11.988 L0.062,9.972 L0.062,15.875 L6.049,15.875 L3.973,13.828 L6.995,10.852 Z" class="si-glyph-fill">
 
-    </path>
-                <path d="M0.961,9.008 L3.058,11.095 L0.005,14.128 L1.885,16.008 L4.942,12.97 L6.909,14.966 L6.909,9.008 L0.961,9.008 Z" class="si-glyph-fill">
+        </path>
+                    <path d="M9.961,0.00800000003 L12.058,2.095 L9.005,5.128 L10.885,7.008 L13.942,3.97 L15.909,5.966 L15.909,0.00800000003 L9.961,0.00800000003 Z" class="si-glyph-fill">
 
-    </path>
+        </path>
+                </g>
             </g>
-        </g>
-    </svg></div>`;
+        </svg></div>`;
 
 let grandir = true;
 
