760ec4c13efce8ebf1ea794c2c16deab1bcaf8e7
ChatPardeur <baptiste.lambert.brunel@gmail.com>
2025-03-28 09:44:19 +0100

    réglé bug épilepsie apparition résumé offre

--- html/js/apparitionOffres.js
+++ html/js/apparitionOffres.js
@@ -2,10 +2,26 @@ console.log("cc")
 let observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
         if(entry.isIntersecting){
-            entry.target.animate([
+            observer.unobserve(entry.target)
+
+            let animation = entry.target.animate([
                 {transform: 'scale(0.7)', opacity: 0.2},
                 {transform: 'scale(1)', opacity: 1}
-            ], {duration: 350});        
+            ], {duration: 350});    
+            
+            
+            
+            animation.onfinish = () =>{
+                let exitObserver = new IntersectionObserver((exitEntries, exitObserver) => {
+                    exitEntries.forEach(exitEntry => {
+                        if (!exitEntry.isIntersecting) { 
+                            observer.observe(exitEntry.target); // Réactive l'observation
+                            exitObserver.unobserve(exitEntry.target); // Arrête l'observation de sortie
+                        }
+                    });
+                });
+                exitObserver.observe(entry.target); // Surveille quand l'élément quitte la vue
+            }
         }
     })
 }, {threshold: 0})
