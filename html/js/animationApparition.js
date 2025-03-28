aba3a8e70eb9b64c209443bfd3e9fd2cffc76ae9
ChatPardeur <baptiste.lambert.brunel@gmail.com>
2025-03-28 12:07:14 +0100

    commenté animationApparition.js

--- html/js/animationApparition.js
+++ html/js/animationApparition.js
@@ -1,16 +1,16 @@
-let observer = new IntersectionObserver((entries) => {
+let observer = new IntersectionObserver((entries) => {              // un observer qui va animer les éléments détectés pour leur apparition
     entries.forEach(entry => {
         if(entry.isIntersecting){
-            observer.unobserve(entry.target)
-
-            let animation = entry.target.animate([
+            observer.unobserve(entry.target)                        // désactive l'observation de l'élément (évite que l'animation ne se joue en boucle
+                                                                    // lorsque l'élément se retouve caché puis réapparaît à cause de l'animation elle même
+            
+            let animation = entry.target.animate([                  // l'animation
                 {transform: 'scale(0.7)', opacity: 0.2},
                 {transform: 'scale(1)', opacity: 1}
             ], {duration: 350});
             
             
-            
-            animation.onfinish = () =>{
+            animation.onfinish = () =>{                             // lorsque l'animation est terminée et que l'élément n'est plus visible, réactive l'observation
                 let exitObserver = new IntersectionObserver((exitEntries, exitObserver) => {
                     exitEntries.forEach(exitEntry => {
                         if (!exitEntry.isIntersecting) { 
@@ -25,7 +25,7 @@ let observer = new IntersectionObserver((entries) => {
     })
 }, {threshold: 0})
 
-switch (document.title) {
+switch (document.title) {                           // définit quels éléments doivent être observés en fonction de la page sur laquelle le script est appelé
     case "Gestion des offres":
         console.log("cc")
         document.querySelectorAll("#conteneurOffre .offre").forEach(elem => {
