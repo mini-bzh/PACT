8e05d791f26ce45cd1d50e3cc190ae9f0a32e8b4
mini_bzh <mini.h.13.9.14.9@gmail.com>
2025-04-03 02:01:25 +0200

    chiffrement de otp

--- html/composants/ajax/affichage_qrcode.php
+++ html/composants/ajax/affichage_qrcode.php
@@ -18,7 +18,19 @@ $stmt->bindParam(':id', $_SESSION["idCompte"], PDO::PARAM_STR);
 $stmt->execute();
 $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
-$secret = $result['secretotp'];
+// Clé secrète (doit être de 32 octets pour AES-256)
+$cle_secrete = file_get_contents("../../.key");
+
+// Décodage de la chaîne base64
+$chiffre_iv_decoded = base64_decode($result['secretotp']);
+
+// Séparer l'IV et le texte chiffré
+$iv_recupere = substr($chiffre_iv_decoded, 0, openssl_cipher_iv_length('aes-256-cbc'));
+$chiffre_recupere = substr($chiffre_iv_decoded, openssl_cipher_iv_length('aes-256-cbc'));
+
+// Déchiffrement
+$secret = openssl_decrypt($chiffre_recupere, 'aes-256-cbc', $cle_secrete, 0, $iv_recupere);
+
 
 $totp = TOTP::create($secret); // On créer l'OTP à partir du secret déjà existant
 $totp->setLabel('PACT');
--- html/composants/ajax/otpValide.php
+++ html/composants/ajax/otpValide.php
@@ -18,7 +18,19 @@
 
     $stmt->execute();
 
-    $secret = $stmt->fetch()["secretotp"];
+    // Clé secrète (doit être de 32 octets pour AES-256)
+    $cle_secrete = file_get_contents("../../.key");
+
+    // Décodage de la chaîne base64
+    $chiffre_iv_decoded = base64_decode($stmt->fetch()['secretotp']);
+
+    // Séparer l'IV et le texte chiffré
+    $iv_recupere = substr($chiffre_iv_decoded, 0, openssl_cipher_iv_length('aes-256-cbc'));
+    $chiffre_recupere = substr($chiffre_iv_decoded, openssl_cipher_iv_length('aes-256-cbc'));
+
+    // Déchiffrement
+    $secret = openssl_decrypt($chiffre_recupere, 'aes-256-cbc', $cle_secrete, 0, $iv_recupere);
+
 
     $totp = TOTP::create($secret);
 
